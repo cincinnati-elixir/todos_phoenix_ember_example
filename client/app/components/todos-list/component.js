443be4271f06692f65b00054c2ec725b02fc6b0b
James Smith <james@gaslight.co>
2015-06-01 23:05:21 -0400

    Connect to our channel:topic as really basic test.

--- client/app/components/todos-list/component.js
+++ client/app/components/todos-list/component.js
@@ -3,4 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'ul',
   channel: Ember.inject.service(),
+
+  didInsertElement: function(){
+    var socket = this.get('channel').socket();
+    socket.connect();
+    var chan = socket.chan("todos:list", {});
+    chan.join().receive("ok", chan => {
+      console.log("Success!");
+    });
+  }
 });
