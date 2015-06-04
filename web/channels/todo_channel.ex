9c1801b3a744a68b8ff25714e6547686e3fd8cb4
James Smith <james@gaslight.co>
2015-06-04 11:17:40 -0400

    Remove unused channel callback functions.

--- web/channels/todo_channel.ex
+++ web/channels/todo_channel.ex
@@ -11,9 +11,9 @@ defmodule TodoChannels.TodoChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
+  #def handle_in("ping", payload, socket) do
+  #  {:reply, {:ok, payload}, socket}
+  #end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (todos:lobby).
